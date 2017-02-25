745eaa10693b133ece3a5559c8b488e9196cb250
seamanhd <seamanhd@gmail.com>
2017-02-25 13:56:43 -0500

    routing from html files

---
+++ routes.js
@@ -0,0 +1,86 @@
+// The url library allows us to parse parts of the request url.
+var url = require("url");
+
+var http = require("http");
+
+var fs = require("fs");
+
+
+
+var PORT = 8080;
+
+var server = http.createServer(handleRequest);
+
+// Lets start our server
+server.listen(PORT, function() {
+  // Callback triggered when server is successfully listening. Hurray!
+  console.log("Server listening on: http://localhost:%s", PORT);
+});
+
+// We need a function which handles requests and send response
+function handleRequest(req, res) {
+
+  // Capturing the url the request is made to.
+  var urlParts = url.parse(req.url);
+
+  // When we visit different urls, the switch statement call on different functions.
+  switch (urlParts.pathname) {
+    case "/":
+      fs.readFile("index.html", function(err,data){
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.end(data);
+      });
+      break;
+    case "/food":
+      fs.readFile("food.html", function(err,data){
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.end(data);
+      });
+      break;
+    case "/frameworks":
+      fs.readFile("frameworks.html", function(err,data){
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.end(data);
+      });
+      break;
+    case "/movies":
+      fs.readFile("movies.html", function(err,data){
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.end(data);
+      });
+      break;
+    default:
+      display404(urlParts.pathname, req, res);
+  }
+}
+
+// When we visit the "http://localhost:8080/" path, this function is run.
+/*function displayRoot(url, req, res) {
+  var myHTML = "<html>";
+  myHTML += "<body><h1>Home Page</h1>";
+  myHTML += "<a href='/portfolio'>Portfolio</a>";
+  myHTML += "</body></html>";
+  res.writeHead(200, { "Content-Type": "text/html" });
+
+  res.end(myHTML);
+}
+
+// When we visit the "http://localhost:8080/portfolio" path, this function is run.
+function displayPortfolio(url, req, res) {
+  var myHTML = "<html>";
+  myHTML += "<body><h1>My Portfolio</h1>";
+  myHTML += "<a href='/'>Go Home</a>";
+  myHTML += "</body></html>";
+  res.writeHead(200, { "Content-Type": "text/html" });
+
+  res.end(myHTML);
+}*/
+
+// When we visit any path that is not specifically defined, this function is run.
+function display404(url, req, res) {
+  res.writeHead(404, {
+    "Content-Type": "text/html"
+  });
+  res.write("<h1>404 Not Found </h1>");
+  res.end("The page you were looking for: " + url + " can not be found ");
+}
